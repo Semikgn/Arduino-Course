bf32f58ce1c66205e5c1a92a8160da13df2e1b22
Semi Kağan <127695077+Semikgn@users.noreply.github.com>
2025-10-11 15:38:57 +0300

    Add files via upload

---
+++ while-for.ino
@@ -0,0 +1,37 @@
+/* FOR LOOP
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  for(int i=0; i<= 10; i++)
+  {
+    Serial.println(i);
+    delay(1000);
+  }
+}*/
+
+
+/* WHILE LOOP 
+
+int armut = 5, elma = 4, kivi = 2;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  while(armut <= 10)
+  {
+    Serial.println("Armut degeri: ");
+    Serial.println(armut);
+    armut++;
+    delay(1000);
+  }
+}
+*/
